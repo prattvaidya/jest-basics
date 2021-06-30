2cbd19280c402acac0b42b3dabbbf8901b22be3b
Pratt Vaidya <pratt.vaidya@gmail.com>
2021-06-30 12:55:00 -0700

    before/after each/all and describe

--- anagram.test.js
+++ anagram.test.js
@@ -1,5 +1,31 @@
 const isAnagram = require('./anagram')
 
+// beforeEach(() => initDatabase())
+// afterEach(() => closeDatabase())
+
+// beforeAll(() => initDatabase())
+// afterAll(() => closeDatabase())
+
+// const initDatabase = () => console.log('Database initialized')
+// const closeDatabase = () => console.log('Database closed')
+
+const anagramCheck = () => console.log("checking name")
+
+// using beforeEach inside describe that it's executed for tests inside this block only
+describe('checking anagrams', () => {
+    beforeEach(() => anagramCheck())
+
+    test('simple test', () => {
+        const user = 'jeff'
+        expect(user).toBe('jeff')
+    })
+    
+    test('simple test 2', () => {
+        const user = 'Karen'
+        expect(user).toBe('Karen')
+    })
+})
+
 test('isAnagram function exists', () => {
     expect(typeof isAnagram).toEqual('function')
 })
