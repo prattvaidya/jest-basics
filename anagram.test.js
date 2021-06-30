1c938215d2037aa8ce12046b8e19d7cfd3754145
Pratt Vaidya <pratt.vaidya@gmail.com>
2021-06-30 12:46:56 -0700

    tests for isAnagram()

---
+++ anagram.test.js
@@ -0,0 +1,18 @@
+const isAnagram = require('./anagram')
+
+test('isAnagram function exists', () => {
+    expect(typeof isAnagram).toEqual('function')
+})
+
+test('"cinema" is an anagram of "iceman"', () => {
+    expect(isAnagram('cinema', 'iceman')).toBeTruthy()
+})
+
+test('"dormitory" is an anagram of "dirty room##"', () => {
+    expect(isAnagram('dormitory', 'dirty room##')).toBeTruthy()
+})
+
+test('"hello" is NOT an anagram of "aloha"', () => {
+    expect(isAnagram('hello', 'aloha')).toBeFalsy()
+})
+
